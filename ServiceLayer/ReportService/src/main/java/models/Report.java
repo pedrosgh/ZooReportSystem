fc2eb6c549425faf2751f36adb6bd0f96087c478
pedrosgh <pedro.rodrigues_essb@hotmail.com>
2020-08-14 15:22:00 +0100

    Creation of the ReportService project and a rough sketch for the Report class implementation.

---
+++ ServiceLayer/ReportService/src/main/java/models/Report.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.Date;
+
+public class Report {
+    private int id;
+    private Date date;
+    private ArrayList<City> cities;
+    private ArrayList<Animal> animals;
+    private double costs;
+    private double gains;
+    private double profit;
+}
